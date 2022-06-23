6e0160f291a0d29368745828491652c0b3882e49
cpyoufy <cpyoufy@gmail.com>
2022-06-23 14:14:29 +0800

    I  ADDED STUFF IM SO PRO DEEZ

---
+++ QuizAppExtraLegsEdition/QuizAppExtraLegsEdition/QuizAppExtraLegsEditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppExtraLegsEditionApp.swift
+//  QuizAppExtraLegsEdition
+//
+//  Created by rgs on 23/6/22.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppExtraLegsEditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
